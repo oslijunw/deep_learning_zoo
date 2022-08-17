04276bb47d48e955327dad0a68c1f00e571e171a
Question Cat <oslijunw@gmail.com>
2022-08-18 00:18:33 +0800

    change: add conv layer size change formula

--- LeNet/model.py
+++ LeNet/model.py
@@ -15,18 +15,20 @@ class LeNet(nn.module):
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x)) # input (3, 32, 32) output ()
-        x = self.pool1(x)
+        # conv layer size formula: 
+        # https://pytorch.org/docs/stable/generated/torch.nn.Conv2d.html#torch.nn.Conv2d
+        x = F.relu(self.conv1(x)) # input (3, 32, 32) output (16, 28, 28)
+        x = self.pool1(x) # output (16, 14, 14)
 
-        x = F.relu(self.conv2(x))
-        x = self.pool2(x)
+        x = F.relu(self.conv2(x)) # output (32, 10, 10)
+        x = self.pool2(x) # output (32, 5, 5)
 
-        x = x.view(-1, 32*5*5)
+        x = x.view(-1, 32*5*5) # output (32*5*5)
 
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc1(x)) # output (120)
+        x = F.relu(self.fc2(x)) # output (84)
 
-        x = self.fc3(x)
+        x = self.fc3(x) # output (10)
         
         return x
 
