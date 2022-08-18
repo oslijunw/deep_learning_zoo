17e0d53e73bb94fa96101a0655a3cdf2f97dc7a3
Question Cat <oslijunw@gmail.com>
2022-08-18 22:28:52 +0800

    Added: finish AlexNet Module framework&train demo

--- AlexNet/model.py
+++ AlexNet/model.py
@@ -2,13 +2,14 @@ import torch.nn as nn
 import torch
 
 class AlexNet(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, init_weight:bool=False, num_classes:int=1000) -> None:
         super().__init__()
         # 提取图片特征
-        self.features = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=48, kernel_size=11, stride=4, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
+        self.backbone = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=48, kernel_size=11, stride=4, padding=2), 
+            # nn.ZeroPad2d((1, 2, 1, 2)), 精准填充
+            nn.ReLU(inplace=True), # inplace是以时间换空间
+            nn.MaxPool2d(kernel_size=3, stride=2), 
             nn.Conv2d(in_channels=48, out_channels=128, kernel_size=5, padding=2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
@@ -22,13 +23,9 @@ class AlexNet(nn.Module):
         )
 
         self.denses = nn.Sequential(
-            # nn.Flatten(),
-
-            # nn.Linear(128*6*6, 4096),
-            # nn.ReLU(inplace=True),
             nn.Dropout(p=.5),
 
-            nn.Linear(256*5*5, 4096),
+            nn.Linear(4608, 4096),
             nn.ReLU(inplace=True),
 
             nn.Dropout(p=.5),
@@ -36,12 +33,29 @@ class AlexNet(nn.Module):
             nn.Linear(4096, 4096),
             nn.ReLU(inplace=True),
 
-            nn.Linear(4096, 1000),
+            nn.Linear(4096, num_classes),
         )
 
+        if init_weight:
+            self._init_weights()
+
     def forward(self, x):
-        x = self.features(x)
+        x = self.backbone(x)
+        # [batch, channel, height, weight]->start_dim=1 
+        # view函数亦可
         x = torch.flatten(x, start_dim=1)
         x = self.denses(x)
-
         return x
+
+    def _init_weights(self):
+        for m in self.modules(): 
+            # Returns an iterator over all modules in the network.
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, .01)
+                nn.init.constant_(m.bias, 0)
+
+
---
+++ AlexNet/train.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+from torchvision import transforms, datasets, utils
+import numpy as np
+import torch.optim as optim
+from model import AlexNet
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+transform_ops = {
+    "train" : transforms.Compose(
+        [   transforms.Resize(224),
+            transforms.RandomCrop(224), # 随机裁剪
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(), # 转化为张量，调整为C H W模式
+            transforms.Normalize((.5, .5, .5), (.5, .5, .5)) # 三通道
+        ],
+    ),
+    "val" : transforms.Compose(
+        [
+            transforms.RandomCrop((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize((.5, .5, .5), (.5, .5, .5))
+        ],
+    ),
+}
+
+train_dataset = datasets.ImageFolder('./data/flowers', transform=transform_ops['train'])
+train_dataLoader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
+
+epochs = 5
+
+model = AlexNet()
+loss_fn = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=1e-3)
+
+for epoch in range(epochs):
+    model.train()
+    epoch_loss = 0
+    train_accurate = 0
+    for step, batch in enumerate(train_dataLoader):
+        batch_acc = 0 
+        images, labels = batch
+        optimizer.zero_grad()
+        pred = model(images)
+        loss = loss_fn(pred, labels)
+        loss.backward()
+        optimizer.step()
+        pred_label = torch.max(pred, dim=1)[1]
+        batch_acc += (pred_label==labels).numpy().sum()/len(images)
+        epoch_loss += loss.item()
+        print(f'epoch {epoch}, step {step} : loss {loss.item()}, batch accuracy {batch_acc}')
+        
+    print(f'epoch acculate loss: {epoch_loss}')
